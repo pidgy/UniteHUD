76de1a678166e82cdae2bad61e2e6f66aa98fd92
pidgy <trashbo4t@protonmail.com>
2026-02-01 09:00:20 -0500

    v4.5.0

---
+++ core/rgba/nrgba/nrgba_test.go
@@ -0,0 +1,31 @@
+package nrgba
+
+import (
+	"fmt"
+	"testing"
+)
+
+func TestWindows(t *testing.T) {
+	m := map[string]NRGBA{
+		"Red":          {R: 255},
+		"Green":        {G: 255},
+		"Blue":         {B: 255},
+		"Pinkity":      Pinkity,
+		"System":       System,
+		"Discord":      Discord,
+		"PastelBlue":   PastelBlue,
+		"PastelCoral":  PastelCoral,
+		"LightPurple":  LightPurple,
+		"Orange":       Orange,
+		"Purple":       Purple,
+		"User":         User,
+		"DarkerYellow": DarkerYellow,
+		"PastelYellow": PastelYellow,
+		"Yellow":       Yellow,
+		"Regice":       Regice,
+	}
+
+	for k, v := range m {
+		fmt.Print(v.Windows(k))
+	}
+}
